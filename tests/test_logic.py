386d81f83440d944d967ec6a1bc14661e0abc08f
ilywka_ <illasalnikov6@gmail.com>
2026-03-11 20:45:11 +0200

    add-test-pacman-lose-lives-games-ends

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,18 +1,33 @@
 import pygame
 from src.utils.constants import TILE_SIZE
+from src.core.game import Game  
 
-def test_pacman_dies_on_ghost_collision(test_pacman, test_ghost):
-    """
-    Тестування зіткнення Пакмена та одного Привида.
+def test_pacman_lose_lives_games_ends(test_pacman, test_ghost):
+    '''
+    Docstring for test_pacman_lose_lives_games_ends
+
+    Tests if games ends when Pacman in collide with Ghost and Pacman has 1 life left.
     
-    Якщо Привид і Пакмен в одному місці - is_dead повинне бути True
-    """
+    :param test_pacman: Test "Fake" Pacman
+    :param test_ghost: Test "Fake" Ghost
+    '''
+    game = Game()
+
+    game.game_state = "game"
     
-    test_pacman.rect.x = 1*TILE_SIZE
-    test_ghost.rect.x = 1*TILE_SIZE
-    test_pacman.rect.y = 1*TILE_SIZE
-    test_ghost.rect.y = 1*TILE_SIZE
+    test_pacman.rect.topleft = (1*TILE_SIZE, 1*TILE_SIZE)
+    test_ghost.rect.topleft = (1*TILE_SIZE, 1*TILE_SIZE)
     
-    is_dead = pygame.sprite.collide_rect(test_pacman, test_ghost)
+    test_pacman.lives = 1
+    test_pacman.shielded = False
+    game.player = test_pacman
+
+    test_ghost.is_scared = True
+    test_ghost.is_dead = False
+    game.ghosts_group = [test_ghost]
+
+    game._handle_collisions()
     
-    assert is_dead == True
+    assert test_pacman.lives == 0
+    assert game.game_state == "lose"
+
