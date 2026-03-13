faa9c3d78d4afac2096a59fac38ca9a11c7d0ae1
anqwampee <anastasiia.pudova@gmail.com>
2026-03-13 20:39:57 +0200

    feat: add tunnel tests

--- tests/test_movement.py
+++ tests/test_movement.py
@@ -1,6 +1,6 @@
 import pytest
 import pygame
-from src.utils.constants import TILE_SIZE, PACMAN_SPEED
+from src.utils.constants import TILE_SIZE, PACMAN_SPEED, WIDTH
 from unittest.mock import MagicMock, patch
 
 
@@ -34,3 +34,25 @@ def test_wall_collision(mock_collision, test_pacman):
 
     assert test_pacman.pos.x == old_x
     assert test_pacman.pos.y == old_y
+
+@patch('src.entities.entity.check_collision', return_value=False)
+def test_tunnel_left(mock_collision, test_pacman):
+    test_pacman.rect.right = -1
+    test_pacman.direction = pygame.Vector2(-1, 0)
+    test_pacman.next_direction = pygame.Vector2(0, 0)
+
+    test_pacman.move()
+
+    assert test_pacman.pos.x == WIDTH + test_pacman.direction.x * test_pacman.speed
+
+
+@patch('src.entities.entity.check_collision', return_value=False)
+def test_tunnel_right(mock_collision, test_pacman):
+    test_pacman.rect.left = WIDTH + 1
+    test_pacman.direction = pygame.Vector2(1, 0)
+    test_pacman.next_direction = pygame.Vector2(0, 0)
+
+    test_pacman.move()
+
+    assert test_pacman.pos.x == -test_pacman.rect.width + test_pacman.direction.x * test_pacman.speed
+
