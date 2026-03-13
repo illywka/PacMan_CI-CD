ac983f5b270901e377a3863994cfc6528cd76b2d
anqwampee <anastasiia.pudova@gmail.com>
2026-03-13 20:16:26 +0200

    feat: add wall collision test

--- tests/test_movement.py
+++ tests/test_movement.py
@@ -5,19 +5,32 @@ from unittest.mock import MagicMock, patch
 
 
 #@pytest.mark.movement
+@patch('src.entities.entity.check_collision', return_value=False)
 @pytest.mark.parametrize("direction, expected_x, expected_y", [
     ((0, -1), TILE_SIZE, TILE_SIZE - PACMAN_SPEED), #вгору
     ((0, 1), TILE_SIZE, TILE_SIZE + PACMAN_SPEED), #вниз
     ((-1, 0), TILE_SIZE - PACMAN_SPEED, TILE_SIZE), #вліво 
     ((1, 0), TILE_SIZE + PACMAN_SPEED, TILE_SIZE)  #вправо
 ])
-def test_movement_directions(test_pacman, direction, expected_x, expected_y):
-    with patch('src.entities.entity.check_collision', return_value=False):
-        test_pacman.direction = pygame.Vector2(direction)
-        test_pacman.next_direction = pygame.Vector2(0, 0)
+def test_movement_directions(mock_collision, test_pacman, direction, expected_x, expected_y):
+    test_pacman.direction = pygame.Vector2(direction)
+    test_pacman.next_direction = pygame.Vector2(0, 0)
 
-        test_pacman.move()
+    test_pacman.move()
 
-        assert test_pacman.pos.x == expected_x
-        assert test_pacman.pos.y == expected_y
-    
+    assert test_pacman.pos.x == expected_x
+    assert test_pacman.pos.y == expected_y
+
+
+@patch('src.entities.entity.check_collision', return_value=True)
+def test_wall_collision(mock_collision, test_pacman):
+    test_pacman.direction = pygame.Vector2(1, 0)
+    test_pacman.next_direction = pygame.Vector2(0, 0)
+
+    old_x = TILE_SIZE 
+    old_y = TILE_SIZE
+
+    test_pacman.move()
+
+    assert test_pacman.pos.x == old_x
+    assert test_pacman.pos.y == old_y
